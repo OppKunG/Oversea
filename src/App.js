4171949e5292567eb9bfb884669e53f938211dd0
OppKunG <67791331+OppKunG@users.noreply.github.com>
2022-09-10 18:04:32 +0700

    Initial

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import React from "react";
 import NavBarRouter from "./navbar-router";
 
 export default function App() {
-  /* 	
-    การทดสอบแต่ละหัวข้อในหนังสือ 
-    จะอยู่ใน comment บล็อกเดียวกัน
-    ดังนั้น หากต้องการทดสอบเรื่องใด 
-    ก็ให้นำ comment ของบล็อกนั้นออกไป
-    แล้วไปทำ comment ในบล็อกเดิมแทน
-    */
-
   // return <NavBar />;
 
-  return <NavBarRouter/>
+  return <NavBarRouter />;
 }
