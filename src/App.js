dfcbc65af294ceec1a76643d41bb572c2183827f
OppKunG <67791331+OppKunG@users.noreply.github.com>
2022-09-11 09:25:56 +0700

    Navbar with Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
-// import NavBar from "./navbar";
-import NavBarRouter from "./navbar-router";
-
+import NavBar from "./Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
 export default function App() {
-  // return <NavBar />;
-
-  return <NavBarRouter />;
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,36 +1,28 @@
-import React from 'react'
-import './navbar.css'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function NavBar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="title">
+        Home
+      </Link>
+      <ul>
+        <CustomLink to="/products">Products</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </nav>
+  );
+}
 
-    return (
-        <nav className="navbar navbar-expand-sm bg-dark px-3 py-0">
-            <a className="navbar-brand" href=" ">
-            <img src="./logo192.png" height="32" alt=""/>
-            </a>
-            <span className="navbar-brand text-info">React</span>
-            <ul className="navbar-nav">
-            <li className="nav-item">
-                <a className="nav-link" href=" ">Home</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href=" ">Products</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href=" ">Member</a>
-            </li>
-            <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href=" " data-bs-toggle="dropdown">
-                    JavaScript
-                </a>
-                <div className="dropdown-menu bg-light">
-                <a className="dropdown-item" href=" ">React</a>
-                <a className="dropdown-item" href=" ">Angular</a>
-                <a className="dropdown-item" href=" ">Vue</a>
-                </div>
-            </li>
-            </ul>
-        </nav>
-    )
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
